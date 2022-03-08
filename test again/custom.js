23dc189f1b438e0bdde342140fee2d4576da90c8
qrescentlight <juventus182@gmail.com>
2022-03-08 16:05:43 +0100

    test more

--- test again/custom.js
+++ test again/custom.js
@@ -2,7 +2,12 @@
 class OneDialog extends HTMLElement {
     connectedCallback() {
 
-        this.innerHTML = `hihi`;
+        this.innerHTML = `<div class="one=dialog">
+        <p><h1>hallo</h1></p>
+        <p>lets go</p>
+    </div>`;
+    document.getElementsByClassName("one-dialog")[1].style.backgroundColor = "yellow";
+
 
     }
 }
